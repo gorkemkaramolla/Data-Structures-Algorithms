ec390be27c90de5d4c8be0bf8353179f0e98989b
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-06-25 00:36:13 +0300

    SelectionSort

---
+++ Chapter4-SortingAlgorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 3, 33, 12, 1, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+        SelectionSort<int>.Sort(array);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
---
+++ Chapter4-SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,28 @@
+class SelectionSort<T> where T : IComparable
+{
+    public static void Sort(T[] array)
+    {
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int minIndex = i;
+            T minValue = array[minIndex];
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j].CompareTo(minValue) < 0)
+                {
+                    minIndex = j;
+                    minValue = array[j];
+
+                }
+            }
+            swap(array, minIndex, i);
+        }
+    }
+    private static void swap(T[] array, int arrayFirst, int arraySmaller)
+    {
+        T temp = array[arrayFirst];
+        array[arrayFirst] = array[arraySmaller];
+        array[arraySmaller] = temp;
+    }
+}
