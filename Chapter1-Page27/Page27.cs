6262a65db5101773f83cfaf4224213916b86932f
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-06-24 17:22:54 +0300

    Chapter3

---
+++ Chapter1-Page27/Page27.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+class Page27
+{
+
+    public static void Main(String[] args)
+    {
+        CultureInfo cultureInfo = new CultureInfo("en-US");
+        Console.Write("The table number: ");
+        string table = Console.ReadLine();
+        Console.Write("The number of people: ");
+        string countString = Console.ReadLine();
+        int.TryParse(countString, out int count);
+        Console.Write("The reservation date (MM/dd/yyyy): ");
+        string dateTimeString = Console.ReadLine();
+        if (!DateTime.TryParseExact(dateTimeString, "M/d/yyyy HH:mm", cultureInfo, DateTimeStyles.None, out DateTime dateTime))
+        {
+            dateTime = DateTime.Now;
+        }
+        Console.WriteLine("Table {0} has been booked for {1} people on {2} at {3}", table, count,
+            dateTime.ToString("M/d/yyyy", cultureInfo),
+            dateTime.ToString("HH:mm", cultureInfo));
+    }
+
+}
---
+++ Chapter2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text;
+class Chapter2
+{
+    public static void Main()
+    {
+        TerrainEnum[,] map = {
+            {
+                TerrainEnum.SAND,TerrainEnum.SAND,TerrainEnum.SAND,
+                TerrainEnum.SAND,TerrainEnum.GRASS,
+                TerrainEnum.SAND,TerrainEnum.GRASS,TerrainEnum.GRASS,
+                TerrainEnum.GRASS,TerrainEnum.GRASS
+            },
+            {
+            TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER,
+            TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER,
+            TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER,
+            TerrainEnum.WATER
+            }
+        };
+        Console.OutputEncoding = Encoding.UTF8;
+        for (int row = 0; row < map.GetLength(0); row++)
+        {
+            for (int column = 0; column < map.GetLength(1); column++)
+            {
+                Console.ForegroundColor = map[row, column].GetColor();
+                Console.Write(map[row, column].GetChar() + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ForegroundColor = ConsoleColor.Gray;
+
+    }
+
+}
