5eccffe72b521d17079cb798f7d9879406a5905f
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-06-26 14:46:20 +0300

    check

---
+++ Variants_of_Trees/BinaryTree.cs
@@ -0,0 +1,32 @@
+public class BinaryTree<T>
+{
+    public BinaryTreeNode<T> Root { get; set; }
+    public int Count { get; set; }
+    private void TraversePreOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+    {
+        if (node != null)
+        {
+            result.Add(node);
+            TraversePreOrder(node.Left, result);
+            TraversePreOrder(node.Right, result);
+        }
+    }
+    private void TraverseInOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+    {
+        if (node != null)
+        {
+            TraverseInOrder(node.Left, result);
+            result.Add(node);
+            TraverseInOrder(node.Right, result);
+        }
+    }
+    private void TraversePostOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+    {
+        if (node != null)
+        {
+            TraversePostOrder(node.Left, result);
+            TraversePostOrder(node.Right, result);
+            result.Add(node);
+        }
+    }
+}
